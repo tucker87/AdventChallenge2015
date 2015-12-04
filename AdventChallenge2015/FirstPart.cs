1cb6c9fc3aaa98fce56e3579b0c84f15c863af09
Jason Tucker <jtgeek@gmail.com>
2015-12-03 23:49:35 -0500

    Oops

--- AdventChallenge2015/FirstPart.cs
+++ AdventChallenge2015/FirstPart.cs
@@ -8,5 +8,29 @@ namespace AdventChallenege2015
 {
     class FirstPart
     {
+        private static void Main(string[] args)
+        {
+            var i = 0;
+            var floor = 0;
+            Console.WriteLine("Enter the code: ");
+            var entry = Console.ReadLine();
+            while (i < entry.Length)
+            {
+                if (entry[i] == '(')
+                {
+                    floor++;
+                }
+                else if (entry[i] == ')')
+                {
+                    floor--;
+                    if (floor == -1)
+                    {
+                        Console.WriteLine("Basement entered at: " + (i + 1));
+                    }
+                }
+                i++;
+            }
+            Console.WriteLine("Floor: " + floor);
+        }
     }
 }
