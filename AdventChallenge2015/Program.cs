e52973b04896b1f884b96d4d04344511d0240eb1
Jason Tucker <jtgeek@gmail.com>
2015-12-04 20:13:04 -0500

    Back to my code.

--- AdventChallenge2015/Program.cs
+++ AdventChallenge2015/Program.cs
@@ -6,68 +6,23 @@ namespace AdventChallenege2015
 {
     internal class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
+            Console.WriteLine(Day1.Solve1(GetInputData().Day1));
+            Console.WriteLine(Day1.Solve2(GetInputData().Day1));
+            Console.WriteLine(Day2.Solve1(GetInputData().Day2));
+            Console.WriteLine(Day2.Solve2(GetInputData().Day2));
+            Console.WriteLine(Day3.Solve1(GetInputData().Day3));
+            Console.WriteLine(Day3.Solve2(GetInputData().Day3));
+            Console.WriteLine(Day4.SolveAsync(GetInputData().Day4, "00000").Result);
+            Console.WriteLine(Day4.SolveAsync(GetInputData().Day4, "000000").Result);
 
-            // The name of the file to open.
-            const string fileName = "advent2Test.txt";
-
-            // This will reference one line at a time
-
-            try
-            {
-                // FileReader reads text files in the default encoding.
-                using (var file = File.OpenText(fileName))
-                {
-                    double totalArea = 0;
-                    string line;
-                    while ((line = file.ReadLine()) != null)
-                    {
-                        var maths = line;
-                        var stringArray = maths.Split('x');
-                        var intArray = new int[stringArray.Length];
-                        for (var i = 0; i < stringArray.Length; i++)
-                        {
-                            var numberAsString = stringArray[i];
-                            intArray[i] = int.Parse(numberAsString);
-                        }
-                        double sideLength = intArray[0];
-                        double sideHeight = intArray[1];
-                        double sideWidth = intArray[2];
-                        double smallestSideArea;
-
-
-                        var side1 = (2*(sideLength*sideWidth));
-                        var side2 = (2*(sideHeight*sideWidth));
-                        var side3 = (2*(sideLength*sideHeight));
-                        if ((side1/2) < (side2/2))
-                        {
-                            smallestSideArea = Math.Min((side1/2), (side3/2));
-                        }
-                        else
-                        {
-                            smallestSideArea = Math.Min((side2/2), (side3/2));
-                        }
-                        var giftArea = (side1 + side2 + side3 + smallestSideArea);
-                        totalArea = totalArea + giftArea;
-                        Console.WriteLine(sideLength + " + " + sideHeight + " + " + sideWidth + " = " + giftArea);
-                        Console.WriteLine(side1 + " + " + side2 + " * " + side3 + " + " + smallestSideArea + " = " +
-                                          giftArea);
-                        Console.WriteLine(totalArea);
-                        Console.WriteLine("\n");
-                    }
-
-                }
-            }
-            catch (FileNotFoundException)
-            {
-                Console.WriteLine("Unable to open file '{0}'", fileName);
-            }
-            catch (IOException ex)
-            {
-                Console.WriteLine("Error reading file '{0}'", fileName);
-            }
             Console.ReadLine();
+        }
+
+        public static JsonInput GetInputData()
+        {
+            return JsonConvert.DeserializeObject<JsonInput>(File.ReadAllText("input.json"));
         }
     }
 }
