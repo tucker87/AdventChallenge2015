66a09a9ebdf4796352bad30d23d3d1f6e7439a83
Jason Tucker <jtgeek@gmail.com>
2015-12-05 13:57:47 -0500

    Refactor

--- AdventChallenge2015/Day5.cs
+++ AdventChallenge2015/Day5.cs
@@ -11,7 +11,6 @@ namespace AdventChallenege2015
 
         public static int Solve1(List<string> input)
         {
-            //return input.Select(Judge).Count(x => x);
             return input.Select(JudgeRegex).Count(x => x);
         }
 
@@ -20,28 +19,6 @@ namespace AdventChallenege2015
             var hasThreeVowels = Regex.Match(child, @"([aeiou].*){3,}").Success;
             var hasDoubleUp = Regex.Match(child, @"(.)\1").Success;
             var hasRestricted = Regex.Match(child, @"(ab|cd|pq|xy)").Success;
-
-            return (hasThreeVowels && hasDoubleUp && !hasRestricted);
-        }
-
-        //Replaced this with Regex version.
-        private static bool Judge(string child)
-        {
-            bool hasDoubleUp = false, hasRestricted = false;
-            
-            var hasThreeVowels = child.Count(chr => Vowels.Contains(chr)) >= 3;
-
-            for (var i = 0; i < child.Length - 1; i++)
-            {
-                if (child[i] == child[i + 1])
-                {
-                    hasDoubleUp = true;
-                    break;
-                }
-            }
-            
-            if (Restricted.Any(child.Contains))
-                hasRestricted = true;
 
             return (hasThreeVowels && hasDoubleUp && !hasRestricted);
         }
@@ -57,6 +34,28 @@ namespace AdventChallenege2015
             var hasSplit = Regex.Match(child, @"(.).\1").Success;
 
             return (hasCouple && hasSplit);
+        }
+
+        //Replaced this with Regex version.
+        private static bool Judge(string child)
+        {
+            bool hasDoubleUp = false, hasRestricted = false;
+
+            var hasThreeVowels = child.Count(chr => Vowels.Contains(chr)) >= 3;
+
+            for (var i = 0; i < child.Length - 1; i++)
+            {
+                if (child[i] == child[i + 1])
+                {
+                    hasDoubleUp = true;
+                    break;
+                }
+            }
+
+            if (Restricted.Any(child.Contains))
+                hasRestricted = true;
+
+            return (hasThreeVowels && hasDoubleUp && !hasRestricted);
         }
     }
 }
